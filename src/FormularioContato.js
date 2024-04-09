9947bb2582a6a18a230aa4526e53ed444094fdc6
Vinicius Herminio <viniciusherminio1@outlook.com>
2024-04-09 11:48:03 -0300

    adicionando regex cpf e whatsapp

--- src/FormularioContato.js
+++ src/FormularioContato.js
@@ -5,14 +5,29 @@ import banner from './images/indiqueeganhe.png'; // Importe a imagem aqui
 
 const FormularioContato = () => {
   const [nome, setNome] = useState('');
-  const [cpf, setcpf] = useState('');
-  const [nomeAmigo, setnomeAmigo] = useState('');
-  const [cpfAmigo, setcpfAmigo] = useState('');
-  const [cidadeAmigo, setcidadeAmigo] = useState('');
-  const [enderecoAmigo, setenderecoAmigo] = useState('');
-  const [whatsappAmigo, setwhatsappAmigo] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [nomeAmigo, setNomeAmigo] = useState('');
+  const [cpfAmigo, setCpfAmigo] = useState('');
+  const [cidadeAmigo, setCidadeAmigo] = useState('');
+  const [enderecoAmigo, setEnderecoAmigo] = useState('');
+  const [whatsappAmigo, setWhatsappAmigo] = useState('');
   const [mensagem, setMensagem] = useState('');
   const [enviado, setEnviado] = useState(false);
+
+  const formatarCPF = (value) => {
+    return value
+      .replace(/\D/g, '') // Remove caracteres não numéricos
+      .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o terceiro dígito
+      .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o sexto dígito
+      .replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Adiciona traço antes dos últimos dois dígitos
+  };
+
+  const formatarTelefone = (value) => {
+    return value
+      .replace(/\D/g, '') // Remove caracteres não numéricos
+      .replace(/(\d{2})(\d)/, '($1) $2') // Adiciona parênteses ao redor dos dois primeiros dígitos
+      .replace(/(\d{5})(\d)/, '$1-$2'); // Adiciona traço após os cinco primeiros dígitos
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -41,22 +56,22 @@ const FormularioContato = () => {
             <input type="text" id="nome" name="nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
 
             <label htmlFor="cpf">CPF do cliente oxente:</label>
-            <input type="text" id="cpf" name="cpf" value={cpf} onChange={(e) => setcpf(e.target.value)} required />
+            <input type="text" id="cpf" name="cpf" value={formatarCPF(cpf)} onChange={(e) => setCpf(e.target.value)} maxLength={14} required />
 
             <label htmlFor="nomeAmigo">Nome do Amigo que irá indicar:</label>
-            <input type="text" id="nomeAmigo" name="nomeAmigo" value={nomeAmigo} onChange={(e) => setnomeAmigo(e.target.value)} required />
+            <input type="text" id="nomeAmigo" name="nomeAmigo" value={nomeAmigo} onChange={(e) => setNomeAmigo(e.target.value)} required />
 
             <label htmlFor="cpfAmigo">CPF do Amigo que irá indicar:</label>
-            <input type="number" id="cpfAmigo" name="cpfAmigo" value={cpfAmigo} onChange={(e) => setcpfAmigo(e.target.value)} required />
+            <input type="text" id="cpfAmigo" name="cpfAmigo" value={formatarCPF(cpfAmigo)} onChange={(e) => setCpfAmigo(e.target.value)} maxLength={14} required />
 
             <label htmlFor="cidadeAmigo">Cidade em que o Amigo que irá indicar reside:</label>
-            <input type="text" id="cidadeAmigo" name="cidadeAmigo" value={cidadeAmigo} onChange={(e) => setcidadeAmigo(e.target.value)} required />
+            <input type="text" id="cidadeAmigo" name="cidadeAmigo" value={cidadeAmigo} onChange={(e) => setCidadeAmigo(e.target.value)} required />
 
             <label htmlFor="enderecoAmigo">Endereço do Amigo que irá indicar:</label>
-            <input type="text" id="enderecoAmigo" name="enderecoAmigo" value={enderecoAmigo} onChange={(e) => setenderecoAmigo(e.target.value)} required />
+            <input type="text" id="enderecoAmigo" name="enderecoAmigo" value={enderecoAmigo} onChange={(e) => setEnderecoAmigo(e.target.value)} required />
             
             <label htmlFor="whatsappAmigo">Whatsapp do Amigo que irá indicar:</label>
-            <input type="enderecoAmigo" id="whatsappAmigo" name="whatsappAmigo" value={whatsappAmigo} onChange={(e) => setwhatsappAmigo(e.target.value)} required />
+            <input type="text" id="whatsappAmigo" name="whatsappAmigo" value={formatarTelefone(whatsappAmigo)} onChange={(e) => setWhatsappAmigo(e.target.value)} maxLength={15} required />
 
             <label htmlFor="mensagem">Mensagem (opcional):</label>
             <textarea id="mensagem" name="mensagem" value={mensagem} onChange={(e) => setMensagem(e.target.value)} rows="4"></textarea>
